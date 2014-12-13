dd67bf7bd71430467f89b60eca6ba1bc6b4ade76
Gerben <gerben@treora.com>
2014-12-13 22:17:20 +0200

    Follow most of the Meteor tutorial
    https://www.meteor.com/try

--- cothink/cothink.js
+++ cothink/cothink.js
@@ -1,23 +1,62 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+    // This code only runs on the client
+    Template.body.helpers({
+        tasks: function () {
+            if (Session.get("hideCompleted")) {
+                // If hide completed is checked, filter tasks
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+            } else {
+                // Otherwise, return all of the tasks
+                return Tasks.find({}, {sort: {createdAt: -1}});
+            }
+        },
+        incompleteCount: function () {
+            return Tasks.find({checked: {$ne: true}}).count();
+        }
+    });
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+    Template.body.events({
+        "change .hide-completed input": function(event) {
+            Session.set("hideCompleted", event.target.checked);
+        },
+        "submit .new-task": function (event) {
+            // This function is called when the new task form is submitted
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+            var text = event.target.text.value;
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date(), // current time
+                owner: Meteor.userId(),           // _id of logged in user
+                username: Meteor.user().username  // username of logged in user
+            });
+
+            // Clear form
+            event.target.text.value = "";
+
+            // Prevent default form submit
+            return false;
+        }
+    });
+
+    Template.task.events({
+        "click .toggle-checked": function() {
+            Tasks.update(this._id, {$set: {checked: ! this.checked}});
+        },
+        "click .delete": function() {
+            Tasks.remove(this._id);
+        }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
 }
 
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+    });
 }
