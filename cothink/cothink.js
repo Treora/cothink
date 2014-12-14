c8e8b9172b129ea5ed19d86a824b01b3fc43f358
Gerben <gerben@treora.com>
2014-12-14 20:24:48 +0200

    Remove things related to completedness of 'tasks'

--- cothink/cothink.js
+++ cothink/cothink.js
@@ -4,23 +4,11 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         tasks: function () {
-            if (Session.get("hideCompleted")) {
-                // If hide completed is checked, filter tasks
-                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-            } else {
-                // Otherwise, return all of the tasks
-                return Tasks.find({}, {sort: {createdAt: -1}});
-            }
+            return Tasks.find({}, {sort: {createdAt: -1}});
         },
-        incompleteCount: function () {
-            return Tasks.find({checked: {$ne: true}}).count();
-        }
     });
 
     Template.body.events({
-        "change .hide-completed input": function(event) {
-            Session.set("hideCompleted", event.target.checked);
-        },
         "submit .new-task": function (event) {
             // This function is called when the new task form is submitted
 
@@ -42,9 +30,6 @@ if (Meteor.isClient) {
     });
 
     Template.task.events({
-        "click .toggle-checked": function() {
-            Tasks.update(this._id, {$set: {checked: ! this.checked}});
-        },
         "click .delete": function() {
             Tasks.remove(this._id);
         }
