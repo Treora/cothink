ce2410c4dd9fecaae3a2ce85696a237f14d496f2
Bart Enkelaar <benkelaar@bol.com>
2015-01-04 14:35:37 +0100

    Fixed indentation of collision function, made delta also dependent on size of collidee and removed alpha parameter

--- cothink/client/layout.js
+++ cothink/client/layout.js
@@ -40,33 +40,36 @@ CoThink.initiateCollision = function () {
 
     function tick(e) {
         nodes
-            .each(collide(.5))
+            .each(collide())
             .style('top', function(a) { return (a.y - a.h / 2) + 'px'; })
             .style('left', function(a) { return (a.x - a.w / 2) + 'px'; });
     }
 
-    function collide(alpha) {
-      var quadtree = d3.geom.quadtree(anchors);
-      return function(a) {
-        nx1 = a.x - a.w / 2,
-        nx2 = a.x + a.w / 2,
-        ny1 = a.y - a.h / 2,
-        ny2 = a.y + a.h / 2;
-        quadtree.visit(function(quad, x1, y1, x2, y2) {
-          if (quad.point && (quad.point !== a)) {
-            var x = a.x - quad.point.x,
-                y = a.y - quad.point.y;
-            if (Math.abs(x) < a.w && Math.abs(y) < a.h) {
-              x *= alpha / a.w;
-              y *= alpha / a.h;
-              a.x += x / 2;
-              a.y += y / 2;
-              quad.point.x -= x / 2;
-              quad.point.y -= y / 2;
-            }
-          }
-          return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-        });
-      };
+    function collide() {
+        var quadtree = d3.geom.quadtree(anchors);
+        return function(a) {
+            nx1 = a.x - a.w / 2,
+            nx2 = a.x + a.w / 2,
+            ny1 = a.y - a.h / 2,
+            ny2 = a.y + a.h / 2;
+            quadtree.visit(function(quad, x1, y1, x2, y2) {
+                if (quad.point && (quad.point !== a)) {
+                    var oa = quad.point,
+                        x = a.x - oa.x,
+                        y = a.y - oa.y,
+                        dw = a.w / 2 + oa.w / 2,
+                        dh = a.h / 2 + oa.h / 2;
+                    if (Math.abs(x) < dw && Math.abs(y) < dh) {
+                        x /= 2 * dw;
+                        y /= 2 * dh;
+                        a.x += x;
+                        a.y += y;
+                        oa.x -= x;
+                        oa.y -= y;
+                    }
+                }
+                return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+            });
+        };
     }
 };
