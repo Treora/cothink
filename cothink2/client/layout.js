fa2f99acf627f106e57a2d5985e84cbe04973400
Gerben <gerben@treora.com>
2015-04-11 12:38:57 +0300

    Create first autorun for updating layout
    Visiting /item/123 triggers 'render' of item 123.

--- cothink2/client/layout.js
+++ cothink2/client/layout.js
@@ -1,7 +1,24 @@
 var Layout = {}
 
-Layout.init = function (msgbus) {
+var autoruns = {
+    'centerFocussedItem': function () {
+        var focussedItem = uiCore.getFocussedItem();
+        if (focussedItem != null) {
+            layout.render(focussedItem);
+        }
+    },
+};
 
+Layout.init = function (msgbus) {
+    for (func in autoruns) {
+        Tracker.autorun(autoruns[func]);
+    }
+};
+
+Layout.render = function (item) {
+    assert_type(item, Item);
+
+    console.log('render item #' + item.id);
 };
 
 layout = Object.create(Layout);
