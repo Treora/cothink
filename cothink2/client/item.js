39a3a848f4393bf43f773c79a077a2de4295ae74
Gerben <gerben@treora.com>
2015-04-11 12:32:05 +0300

    Add assert, pass around Item objects

--- cothink2/client/item.js
+++ cothink2/client/item.js
@@ -1,1 +1,17 @@
 Item = {};
+
+items = (function () {
+
+    var getItemById = function (id) {
+        item = Object.create(Item);
+        item.id = id;
+        item.title = 'lalala';
+        item.content = 'bla, my id is ' + id + '.';
+        return item;
+    };
+
+    return {
+        getItemById: getItemById,
+    };
+
+})();
--- cothink2/client/uicore.js
+++ cothink2/client/uicore.js
@@ -1,7 +1,8 @@
 
 var UICoreEvents = {
     'routedToItem': function (itemId) {
-        this.setFocussedItem(itemId);
+        var item = items.getItemById(itemId);
+        this.setFocussedItem(item);
     }
 };
 
@@ -13,8 +14,25 @@ UICore.init = function (msgbus) {
     msgbus.on(UICoreEvents, this);
 };
 
-UICore.setFocussedItem = function (itemId) {
-    this.state.set('focussedItem', itemId);
+UICore.setFocussedItem = function (item) {
+    if (item == null) {
+        this.state.set('focussedItem', null);
+    }
+    else {
+        assert_type(item, Item);
+        itemId = item.id;
+        this.state.set('focussedItem', itemId);
+    }
+};
+
+UICore.getFocussedItem = function () {
+    var id = this.state.get('focussedItem');
+    if (id == null) {
+        return null;
+    }
+    else {
+        return items.getItemById(id);
+    }
 };
 
 uiCore = Object.create(UICore);
