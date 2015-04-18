758b86c645fd9fce1d4161e68c547888f8247f1d
Gerben <gerben@treora.com>
2015-04-18 16:17:49 +0300

    Move itemManager into own file

--- cothink2/client/item.js
+++ cothink2/client/item.js
@@ -1,17 +1,1 @@
 Item = {};
-
-itemManager = (function () {
-
-    var getItemById = function (id) {
-        var itemData = itemsCollection.findOne(id);
-        var item = Object.create(Item);
-        item.id = id;
-        _.extend(item, itemData);
-        return item;
-    };
-
-    return {
-        getItemById: getItemById,
-    };
-
-})();
---
+++ cothink2/client/itemmanager.js
@@ -0,0 +1,15 @@
+itemManager = (function () {
+
+    var getItemById = function (id) {
+        var itemData = itemsCollection.findOne(id);
+        var item = Object.create(Item);
+        item.id = id;
+        _.extend(item, itemData);
+        return item;
+    };
+
+    return {
+        getItemById: getItemById,
+    };
+
+})();
