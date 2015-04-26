30e5ee5a76381a1c054027e41165bbcf2bb93300
Gerben <gerben@treora.com>
2015-04-26 19:09:28 +0300

    Keep item data in Item.getData
    Abandon the pattern introduced earlier; a function like getVisibleItems
    should not be reactive to both the selection of items and the items'
    contents, because changing an item's content should only trigger an
    update for that single item. Getting the content should be done in a
    separate autorun (by the template/Blaze).

--- cothink2/client/item.js
+++ cothink2/client/item.js
@@ -3,3 +3,7 @@ Item = {};
 Item.isFocussed = function () {
     return uiCore.state.equals('focussedItem', this.id);
 };
+
+Item.getData = function () {
+    return itemsCollection.findOne(this.id);
+};
--- cothink2/client/itemmanager.js
+++ cothink2/client/itemmanager.js
@@ -1,10 +1,8 @@
 itemManager = (function () {
 
     var getItemById = function (id) {
-        var itemData = itemsCollection.findOne(id);
         var item = Object.create(Item);
         item.id = id;
-        _.extend(item, itemData);
         return item;
     };
 
